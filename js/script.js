23f1150cd15f0d037540cae498740fa7a6059028
bassiebangura <devbassiebangura@gmail.com>
2019-06-12 22:42:55 -0400

    Create working display page function.

--- js/script.js
+++ js/script.js
@@ -2,49 +2,32 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+const listOfStudents = document.querySelectorAll(".student-item");
+const numberOfStudentsToDisplay = 10;
+console.log(listOfStudents);
+console.log(typeof listOfStudents);
 
+let displayPage = (list, page) => {
+  /*function evlautes start and end index from list
+   for items and display items within range.
+   */
+  const listOfStudentsContainer = document.querySelector("ul.student-list");
+  listOfStudentsContainer.innerHTML = " ";
+  let startIndex = page * numberOfStudentsToDisplay - numberOfStudentsToDisplay;
+  let endIndex = page * numberOfStudentsToDisplay;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+  list.forEach((item, index) => {
+    if (index >= startIndex && index <= endIndex) {
+      console.log(index);
+      console.log(item);
+       listOfStudentsContainer.appendChild(item);
+    }
+  });
+};
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
+displayPage(listOfStudents, 1);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
