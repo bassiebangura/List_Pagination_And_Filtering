fac958a5b3b610330a2426dd81363c2ac4bd5d33
bassiebangura <devbassiebangura@gmail.com>
2019-06-17 19:35:37 -0400

    Refactor eventListener for searchButton.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const numberOfStudentsToDisplay = 10;
 const pageHeaderContainer = document.querySelector("div .page-header"); //get header container
 const pageContainer = document.querySelector("body .page");
 
-
 //helper function
 let createAnElement = elementType => {
   //function to create HTML elements
@@ -22,12 +21,14 @@ searchFilterContainer.setAttribute("class", "student-search");
 let searchFilterContainerSearchBox = createAnElement("input"); //searchFilterContainerContainer searchBox
 let searchFilterContainerSearchBoxButton = createAnElement("BUTTON"); //search button
 searchFilterContainerSearchBoxButton.textContent = "Search";
-searchFilterContainerSearchBox.setAttribute("placeholder", "Search for students...");
+searchFilterContainerSearchBox.setAttribute(
+  "placeholder",
+  "Search for students..."
+);
 
 searchFilterContainer.appendChild(searchFilterContainerSearchBox);
 searchFilterContainer.appendChild(searchFilterContainerSearchBoxButton);
 pageHeaderContainer.appendChild(searchFilterContainer);
-
 
 let displayPage = (list, page = 1) => {
   /*function evlautes start and end index an display  10 items within range.
@@ -44,9 +45,7 @@ let displayPage = (list, page = 1) => {
   });
 };
 
-
-
-let appendPageLinks = (totalItems) => {
+let appendPageLinks = totalItems => {
   /*
    function used to generate, append, and add 
    evenListeners to the pagination buttons.
@@ -73,19 +72,19 @@ let appendPageLinks = (totalItems) => {
   paginationDivContainer.innerHTML += "</ul>";
   let paginationListUl = document.querySelector(".pagination ul");
   let paginationListLinks = document.querySelectorAll(".pagination ul li a");
-  
+
   paginationListUl.addEventListener("click", el => {
     /*add click event to paginationList 'ul' and use event bubbling
     to respond to click events on pagination list page numbers.
     */
     paginationListLinks.forEach(item => {
-       //use .forEach to remove the 'active' class
+      //use .forEach to remove the 'active' class
       if (item.classList.length) {
         item.classList = " ";
       }
     });
 
-    el.target.classList += "active";//add active class to target element
+    el.target.classList += "active"; //add active class to target element
     displayPage(listOfStudents, el.target.textContent);
   });
 };
@@ -94,71 +93,54 @@ let appendPageLinks = (totalItems) => {
 let searchButton = document.querySelector("button");
 let searchBox = document.querySelector("input");
 
-
 searchBox.addEventListener("keyup", () => {
-  let searchBoxValue, studentName, i, txtValue,searchResultsArray = [];
-  let paginationDivContainer = document.querySelector(".pagination")
+  let searchBoxValue,
+    studentName,
+    i,
+    txtValue,
+    searchResultsArray = [];
+  let paginationDivContainer = document.querySelector(".pagination");
 
-
-  if(paginationDivContainer) {
+  if (paginationDivContainer) {
     //remove if already on page else add new one base on filter results.
-    pageContainer.removeChild(paginationDivContainer)
+    pageContainer.removeChild(paginationDivContainer);
   }
   searchBoxValue = searchBox.value.toUpperCase();
   for (i = 0; i < listOfStudents.length; i++) {
     studentName = listOfStudents[i].getElementsByTagName("h3")[0];
     txtValue = studentName.textContent;
     if (txtValue.toUpperCase().indexOf(searchBoxValue) > -1) {
-      searchResultsArray.push(listOfStudents[i])
-    } 
-    
+      searchResultsArray.push(listOfStudents[i]);
+    }
   }
-  displayPage(searchResultsArray)
+  displayPage(searchResultsArray);
   appendPageLinks(searchResultsArray);
-}); 
-
-displayPage(listOfStudents);//creates default page when js is enabled
-appendPageLinks(listOfStudents);//append default page links when js is enabled.
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 //search button implementation
 searchButton.addEventListener("click", () => {
-  let searchBoxValue, studentName, i, txtValue, searchResultsArray;
+  let searchBoxValue,
+    studentName,
+    i,
+    txtValue,
+    searchResultsArray = [];
+  let paginationDivContainer = document.querySelector(".pagination");
+
+  if (paginationDivContainer) {
+    //remove if already on page else add new one base on filter results.
+    pageContainer.removeChild(paginationDivContainer);
+  }
   searchBoxValue = searchBox.value.toUpperCase();
   for (i = 0; i < listOfStudents.length; i++) {
-    //console.log(list[i])
     studentName = listOfStudents[i].getElementsByTagName("h3")[0];
-    //console.log(studentName)
     txtValue = studentName.textContent;
-
     if (txtValue.toUpperCase().indexOf(searchBoxValue) > -1) {
-      listOfStudents[i].style.display = "";
-      searchResultsArray.push(listOfStudents[i])
-      console.log(searchResultsArray)
-    } else {
-      listOfStudents[i].style.display = "none";
+      searchResultsArray.push(listOfStudents[i]);
     }
   }
-  // let filteredListOfStudents = document.querySelectorAll(".student-item");
-  // console.log("hello");
-  // console.log(filteredListOfStudents);*9+-*6
-  // console.log(filteredListOfStudents.length);
-  // appendPageLinks(filteredListOfStudents);
-  //document.querySelector("pagination");
-  //appendPageLinks(filterListOfStudents);
-  //1. add all students whose name meet criteria to student-list container.
-  //2. call pagination on your results
+  displayPage(searchResultsArray);
+  appendPageLinks(searchResultsArray);
 });
-//displayPage(listOfStudents, 1); //default page is number 1
-//appendPageLinks(listOfStudents); //call function to add pagination numbers
+
+displayPage(listOfStudents); //creates default page when js is enabled
+appendPageLinks(listOfStudents); //append default page links when js is enabled.
